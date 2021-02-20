d1bbbf055bfcf1f1d4f0a1f41d9e22e37fcf5114
Dhananjay Dubey <ddubey330@gmail.com>
2021-02-20 10:59:29 +0800

    Initial Project Setup

---
+++ TrafficCamera/TrafficCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficCamera
+//
+//  Created by Dhananjay Dubey on 20/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
